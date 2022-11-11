40a0fd90a9d8db0a3c61922937fe055ad8a7a826
TanzinAhammad <tanzin26337@gmail.com>
2022-11-11 23:52:37 +0600

    commit 1

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { DialogComponent } from '../dialog/dialog.component';
+
+
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+
+
+  ngOnInit(): void {
+  }
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+       width: "30%"
+    });
+  }
+
+}
